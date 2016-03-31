d0c72feedc1ad0970f3078c2f94dd4d5fa0cd6a4
deepwaterooo <heyanh@vandals.uidaho.edu>
2016-03-31 09:17:09 -0700

    video

--- FirstApp/src/dev/firstapp/IOpenGLDemo.java
+++
@@ -1,7 +0,0 @@
-package dev.firstapp;
-
-import javax.microedition.khronos.opengles.GL10;
-
-public interface IOpenGLDemo {
-    public void DrawScene(GL10 gl);
-}
--- README.md
+++
@@ -1,8 +0,0 @@
-10/20/014
-
-working the opengl graphics 
-before
-draw a line first
-
-then will work on 
-draw a line first
